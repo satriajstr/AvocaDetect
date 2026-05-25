8e636409ad317e87030f1e0201213c69258a69b4
satriajstr <satriastar77@gmail.com>
2026-05-25 20:03:56 +0700

    Julius: Merapihkan pre-processing

--- backend/machine/preprocessing/grayscale.py
+++ backend/machine/preprocessing/grayscale.py
@@ -2,7 +2,7 @@ import numpy as np
 
 def to_grayscale(image):
     """
-    Konversi gambar BGR (hasil cv2.imread) ke grayscale secara manual.
+    Konversi gambar BGR (hasil cv2.imread) ke grayscale secara manual
     Menggunakan rumus luminansi standar (ITU-R 601-2 Luma):
     Y = 0.299 * R + 0.587 * G + 0.114 * B
     """
